deebdf05ac0650ec47ead0c3655aa2d3b650ea0c
Erkhan <erkhanpiriyev@gmail.com>
2025-01-28 08:33:06 +0500

    fix

--- src/Classes/Doctor.java
+++ src/Classes/Doctor.java
@@ -5,43 +5,38 @@ import java.util.*;
 
 public class Doctor extends Person {
     private Map<DayOfWeek, Set<SessionTime>> schedule = new HashMap<>();
-    private Set<DayOfWeek> restrictedDays = new HashSet<>();
 
     public Doctor() {
+        initializeSchedule();
     }
 
     public Doctor(String name, String surname) {
         super(name, surname);
+        initializeSchedule();
     }
 
-    public void restrictDay(DayOfWeek day) {
-        restrictedDays.add(day);
-        schedule.remove(day);
-    }
-
-    private void validateDay(DayOfWeek day) {
-        if (restrictedDays.contains(day)) {
-            throw new IllegalArgumentException("Sessions are not allowed on " + day);
+    private void initializeSchedule() {
+        for (DayOfWeek day : DayOfWeek.values()) {
+            schedule.put(day, new HashSet<>());
         }
     }
 
+    public void restrictDay(DayOfWeek day) {
+        schedule.remove(day);
+    }
+
     public void addSession(DayOfWeek day, SessionTime sessionTime) {
-        validateDay(day);
-        schedule.putIfAbsent(day, new HashSet<>());
         schedule.get(day).add(sessionTime);
     }
 
     public void removeSession(DayOfWeek day, SessionTime sessionTime) {
         if (schedule.containsKey(day)) {
             schedule.get(day).remove(sessionTime);
-            if (schedule.get(day).isEmpty()) {
-                schedule.remove(day);
-            }
         }
     }
 
     public boolean isWorkingOn(DayOfWeek day) {
-        return schedule.containsKey(day) && !schedule.get(day).isEmpty();
+        return schedule.containsKey(day);
     }
 
     public Set<SessionTime> getSessions(DayOfWeek day) {
@@ -52,18 +47,22 @@ public class Doctor extends Person {
         return schedule.keySet();
     }
 
+
     @Override
     public String toString() {
         StringBuilder scheduleString = new StringBuilder(super.toString() + "   Schedule:\n");
         for (Map.Entry<DayOfWeek, Set<SessionTime>> entry : schedule.entrySet()) {
-            scheduleString.append("      " + entry.getKey()).append(": ");
-            for (SessionTime session : entry.getValue()) {
-                scheduleString.append(session).append(", ");
+            scheduleString.append("      ").append(entry.getKey()).append(": ");
+            if (entry.getValue().isEmpty()) {
+                scheduleString.append("No sessions");
+            } else {
+                for (SessionTime session : entry.getValue()) {
+                    scheduleString.append(session).append(", ");
+                }
+                scheduleString.setLength(scheduleString.length() - 2);
             }
-            scheduleString.setLength(scheduleString.length() - 2);
             scheduleString.append("\n");
         }
         return scheduleString.toString();
     }
-
 }
